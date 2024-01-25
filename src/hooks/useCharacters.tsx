45215d3636a1cd3853fd9c5bf867f75e23ae66c9
Edgar Chaver <edieveg316@gmail.com>
2024-01-24 20:57:21 -0600

    Create useCharacters.tsx

---
+++ src/hooks/useCharacters.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios, { AxiosError } from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../redux/store";
+import { fetchCharacters } from "../redux/actions/characterActions";
+import { CharactersResponse } from "../models/Character";
+import { BASE_URL } from "../redux/constants";
+
+export const useCharacters = () => {
+	const dispatch = useDispatch<AppDispatch>();
+	const characters = useSelector((state: RootState) => state.characters);
+
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string>("");
+	const [charactersData, setCharactersData] = useState<CharactersResponse | null>(null);
+
+	useEffect(() => {
+		const fetchCharactersData = async () => {
+			try {
+				const response = await axios.get<CharactersResponse>(`${BASE_URL}/character`);
+				setCharactersData(response.data);
+				dispatch(fetchCharacters());
+			} catch (error) {
+				const axiosError = error as AxiosError;
+				setError(axiosError.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchCharactersData();
+	}, [dispatch]);
+	return { loading, error, charactersData, characters };
+};
