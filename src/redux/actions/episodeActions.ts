e3a2bafbb146ffc5704d94e27450057780fa3c35
Edgar Chaver <edieveg316@gmail.com>
2024-01-31 14:28:37 -0600

    Create useEpisodes hook
    Update reducers and actions for episodes

--- src/redux/actions/episodeActions.ts
+++ src/redux/actions/episodeActions.ts
@@ -4,10 +4,13 @@ import { RootState } from "../store";
 import {
 	FETCH_EPISODES_START,
 	FETCH_EPISODES_SUCCESS,
+	FETCH_EPISODES_RESPONSE,
 	FETCH_EPISODES_FAILURE,
 	BASE_URL,
 } from "../constants";
-import { EpisodesResponse } from "../../models/Episode";
+import { EpisodesResponse, EpisodeSchema } from "../../models/Episode";
+
+export type ThunkResult<R> = ThunkAction<R, RootState, unknown, EpisodesAction>;
 
 interface FetchEpisodesStartAction {
 	type: typeof FETCH_EPISODES_START;
@@ -15,6 +18,11 @@ interface FetchEpisodesStartAction {
 
 interface FetchEpisodesSuccessAction {
 	type: typeof FETCH_EPISODES_SUCCESS;
+	payload: EpisodeSchema[];
+}
+
+interface FetchEpisodesResponseAction {
+	type: typeof FETCH_EPISODES_RESPONSE;
 	payload: EpisodesResponse;
 }
 
@@ -26,19 +34,18 @@ interface FetchEpisodesFailureAction {
 export type EpisodesAction =
 	| FetchEpisodesStartAction
 	| FetchEpisodesSuccessAction
+	| FetchEpisodesResponseAction
 	| FetchEpisodesFailureAction;
 
-export const fetchEpisodes = (): ThunkAction<
-	Promise<void>,
-	RootState,
-	unknown,
-	EpisodesAction
-> => {
+export const fetchEpisodes = (
+	episodes: EpisodeSchema[]
+): ThunkAction<Promise<void>, RootState, unknown, EpisodesAction> => {
 	return async dispatch => {
 		dispatch({ type: FETCH_EPISODES_START });
 		try {
 			const response = await axios.get<EpisodesResponse>(`${BASE_URL}/episode`);
-			dispatch({ type: FETCH_EPISODES_SUCCESS, payload: response.data });
+			dispatch({ type: FETCH_EPISODES_RESPONSE, payload: response.data });
+			dispatch({ type: FETCH_EPISODES_SUCCESS, payload: episodes });
 		} catch (error) {
 			const err = error as AxiosError;
 			dispatch({ type: FETCH_EPISODES_FAILURE, payload: err.message });
--- src/redux/reducers/episodesReducers.ts
+++ src/redux/reducers/episodesReducers.ts
@@ -1,6 +1,7 @@
 import {
 	FETCH_EPISODES_START,
 	FETCH_EPISODES_SUCCESS,
+	FETCH_EPISODES_RESPONSE,
 	FETCH_EPISODES_FAILURE,
 } from "../constants";
 import { EpisodesResponse } from "../../models/Episode";
@@ -9,17 +10,22 @@ interface EpisodesState {
 	loading: boolean;
 	episodes: EpisodesResponse | null;
 	error: string | null;
+	pages: number;
+	count: number;
 }
 
 export const initialState: EpisodesState = {
 	loading: false,
 	episodes: null,
 	error: null,
+	pages: 0,
+	count: 0,
 };
 
 export type EpisodesAction =
 	| { type: typeof FETCH_EPISODES_START }
 	| { type: typeof FETCH_EPISODES_SUCCESS; payload: EpisodesResponse }
+	| { type: typeof FETCH_EPISODES_RESPONSE; payload: EpisodesResponse }
 	| { type: typeof FETCH_EPISODES_FAILURE; payload: string };
 
 export const episodesReducer = (
@@ -37,6 +43,13 @@ export const episodesReducer = (
 				...state,
 				loading: false,
 				episodes: action.payload,
+			};
+		case FETCH_EPISODES_RESPONSE:
+			return {
+				...state,
+				loading: false,
+				pages: action.payload.info.pages ?? 0,
+				count: action.payload.info.count ?? 0,
 			};
 		case FETCH_EPISODES_FAILURE:
 			return {
